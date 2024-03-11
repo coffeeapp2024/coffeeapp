5ff2a757d70cd897ecc15795830b7e52eae5885e
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-12 02:14:23 +0700

    e

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Logo() {
   return (
-    <div className="mb-20">
+    <div className="mb-24">
       <h1 className="text-4xl font-extrabold text-neutral-800 w-fit mx-auto">
         The Coffee House
       </h1>
--- components/ScanClaimCard.tsx
+++ components/ScanClaimCard.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 function ScanClaimCard() {
   return (
-    <div className="absolute bottom-1/3 w-full px-3">
+    <div className="absolute bottom-1/4 sm:bottom-1/3 w-full px-3">
       <div className=" bg-white bg-opacity-90 rounded-3xl border-[1px] border-neutral-300 flex px-3 py-3 items-center justify-between shadow-sm">
         <div className="flex items-center gap-x-2">
           <div>
