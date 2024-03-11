3121a2c4bcbb2a18683a18df6aa340cebb0284a1
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-12 02:00:26 +0700

    e

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -4,23 +4,17 @@ import ProfileInfo from "./ProfileInfo";
 import { useEffect, useState } from "react";
 import { auth } from "@/lib/firebase";
 import { User } from "firebase/auth";
-import Link from "next/link";
 import LoginButton from "./LoginButton";
 
 function ProfileCard() {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    // Set up an observer for changes in authentication state
     const unsubscribe = auth.onAuthStateChanged((user) => {
-      setUser(user); // Update the user state when authentication state changes
+      setUser(user);
     });
-
-    // Clean up the observer when the component unmounts
     return () => unsubscribe();
-  }, []); // Only run this effect once when the component mounts
-
-  console.log("user:", user);
+  }, []);
 
   return (
     <div className="mx-3 h-16 bg-white bg-opacity-90 rounded-3xl border-[1px] border-neutral-300 shadow-sm active:scale-[98%] transition-transform">
