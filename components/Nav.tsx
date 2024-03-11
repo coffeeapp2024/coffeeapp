c16623108b8d0b5ae9e570b556b5c78d9c19c6de
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-12 01:18:32 +0700

    e

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,7 +9,7 @@ import React from "react";
 
 function Nav() {
   return (
-    <div className="fixed bottom-10 w-full max-w-screen-sm z-10">
+    <div className="fixed bottom-10 w-full max-w-screen-sm z-10 shadow-sm">
       <div className="flex items-center justify-center w-fit mx-auto px-3 py-2 bg-white bg-opacity-90 rounded-3xl border-[1px] border-neutral-300 divide-neutral-300  divide-x-[1px]">
         <Link href="/">
           <div className="nav-item">
