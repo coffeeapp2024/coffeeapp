4e1ab91902d49d97166dece6ac4697c7a02a2e29
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-12 22:36:54 +0700

    e

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,43 @@
 import AboutImageList from "@/components/AboutImageList";
+import Video from "@/components/Video";
+import { CheckIcon } from "@heroicons/react/24/outline";
 import React from "react";
 
 function page() {
+  const textList = [
+    "Lorem ipsum, dolor sit amet consectetur",
+    "Ipsum, dolor sit amet consectetur",
+    "Lorem ipsum, dolor sit amet consectetur",
+    "Lorem ipsum, dolor sit ",
+  ];
+
   return (
     <div className="bg-neutral-950 min-h-screen text-white">
-      <AboutImageList />
+      <Video url="https://www.youtube.com/watch?v=i8_X77znqxg" />
+      <div className="pt-2 px-2">
+        <h1 className="font-bold text-3xl mb-2">The Coffee House</h1>
+        <div className="text-neutral-500 mb-4">
+          <p className="">Address: Landmark 18 Bình Thạnh, Hồ Chí Minh</p>
+          <p>Open: 24/7</p>
+        </div>
+        <div className="rounded-2xl pt-6 pb-8 bg-neutral-800 mb-6">
+          <div className="pb-4 border-b-[1px] border-opacity-20 border-white px-3 text-opacity-80 mb-6">
+            <h3 className="font-bold text-xl">Why should have a sip here?</h3>
+          </div>
+          <div className=" pl-4 pr-1 flex flex-col gap-y-2">
+            {textList.map((text, index) => (
+              <div
+                key={index}
+                className="flex items-center justify-start gap-x-2"
+              >
+                <CheckIcon className="w-6 h-6 text-green-500" />
+                <p className="font-medium">{text}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <AboutImageList />
+      </div>
     </div>
   );
 }
--- components/AboutImageList.tsx
+++ components/AboutImageList.tsx
@@ -5,7 +5,7 @@ function AboutImageList() {
   const imageUrlList = ["/img/cf1.jpg", "/img/cf2.jpg", "/img/cf3.jpg"];
 
   return (
-    <div className="px-3 flex flex-col gap-y-3">
+    <div className="px-1 flex flex-col gap-y-3">
       {imageUrlList.map((img_url, index) => (
         <AboutImage key={index} img_url={img_url} />
       ))}
