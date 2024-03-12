7f82a1c5d241e20ca8453199809d4145fef44b6f
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-12 22:45:44 +0700

    e

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
     const intervalId = setInterval(() => {
       const randomIndex = Math.floor(Math.random() * bgList.length);
       setBg(bgList[randomIndex]);
-    }, 10000); // Change background every 1 minute (60000 milliseconds)
+    }, 10000);
     return () => clearInterval(intervalId);
   }, []);
 
