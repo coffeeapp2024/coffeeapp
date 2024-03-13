f27917d666bb8f0898877f4560b82c1c39a44dc7
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-13 20:41:39 +0700

    e

--- components/Boost.tsx
+++ components/Boost.tsx
@@ -1,27 +1,34 @@
 import Image from "next/image";
 import React from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 function Boost() {
   return (
-    <div className="relative active:scale-95 duration-75 transition-transform">
-      <div className="shadow-lg rounded-[30px] aspect-[6/7] flex items-center justify-center overflow-hidden">
-        <Image
-          src={"/icons/rocket.gif"}
-          layout={"responsive"}
-          height={400}
-          width={100}
-          alt="boost"
-          unoptimized={true}
-          className="scale-[1.6]"
-        />
-      </div>
-      <div className="absolute -bottom-4 left-1/2 -translate-x-1/2 shadow-lg">
-        <div className="bg-gradient-to-tr from-amber-300 to-amber-400 border-neutral-800 border-[1px] px-4 py-2 rounded-xl text-nowrap font-bold text-xl">
-          Boost
+    <Dialog>
+      <DialogTrigger>
+        {" "}
+        <div className="relative active:scale-95 duration-75 transition-transform">
+          <div className="shadow-lg rounded-[30px] aspect-[6/7] flex items-center justify-center overflow-hidden">
+            <Image
+              src={"/icons/rocket.gif"}
+              layout={"responsive"}
+              height={400}
+              width={100}
+              alt="boost"
+              unoptimized={true}
+              className="scale-[1.6]"
+            />
+          </div>
+          <div className="absolute -bottom-4 left-1/2 -translate-x-1/2 shadow-lg">
+            <div className="bg-gradient-to-tr from-amber-300 to-amber-400 border-neutral-800 border-[1px] px-4 py-2 rounded-xl text-nowrap font-bold text-xl">
+              Boost
+            </div>
+            <div className="absolute bg-neutral-950 w-full h-full rounded-xl -z-10 top-[3px] left-1 "></div>
+          </div>
         </div>
-        <div className="absolute bg-neutral-950 w-full h-full rounded-xl -z-10 top-[3px] left-1 "></div>
-      </div>
-    </div>
+      </DialogTrigger>
+      <DialogContent className="max-w-sm min-h-96 rounded-2xl"></DialogContent>
+    </Dialog>
   );
 }
 
