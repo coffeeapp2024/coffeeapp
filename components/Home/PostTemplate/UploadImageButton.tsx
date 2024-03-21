59b063d34d8b13969794a69d1b868201ff2053e5
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-21 17:06:57 +0700

    e

--- components/Home/PostTemplate/UploadImageButton.tsx
+++ components/Home/PostTemplate/UploadImageButton.tsx
@@ -32,7 +32,7 @@ function UploadImageButton({ onFileSelect }: Props) {
         accept="image/*"
       />
       <button
-        className="bg-neutral-800 w-full text-sm text-white font-semibold mx-6 rounded-3xl px-3 py-2 flex items-center justify-center"
+        className="bg-neutral-800 w-full text-sm text-white font-semibold mx-2 rounded-3xl px-3 py-3 flex items-center justify-center"
         onClick={handleButtonClick}
       >
         Upload Your Image
