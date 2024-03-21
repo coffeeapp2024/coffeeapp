6b3594be3d556f45a40353449d4f44f43cdb8e12
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-21 17:04:05 +0700

    e

--- components/Home/PostTemplate/UploadImageButton.tsx
+++ components/Home/PostTemplate/UploadImageButton.tsx
@@ -23,16 +23,16 @@ function UploadImageButton({ onFileSelect }: Props) {
   };
 
   return (
-    <div>
+    <div className="w-full flex items-center justify-center">
       <input
         type="file"
         ref={hiddenFileInput}
         onChange={handleFileChange}
-        style={{ display: "none" }}
+        className="hidden"
         accept="image/*"
       />
       <button
-        className="bg-neutral-800 w-full text-white font-semibold mx-4 rounded-3xl px-3 py-3 flex items-center justify-center"
+        className="bg-neutral-800 w-full text-sm text-white font-semibold mx-6 rounded-3xl px-3 py-2 flex items-center justify-center"
         onClick={handleButtonClick}
       >
         Upload Your Image
