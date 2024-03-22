dfe613d9d9689da7cae82d119b2d5230a5cf8188
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-23 04:20:42 +0700

    e

--- components/Mine/Profile/Admin.tsx
+++ components/Mine/Profile/Admin.tsx
@@ -4,10 +4,16 @@ import React from "react";
 function Admin() {
   return (
     <div className="absolute bottom-16 left-1/2 -translate-x-1/2 text-neutral-400 text-sm font-semibold flex gap-x-2">
-      <Link href="/staff" className="p-2 border-2 rounded-3xl">
+      <Link
+        href="/staff"
+        className="p-2 border-2 border-neutral-300 rounded-3xl"
+      >
         Staff
       </Link>
-      <Link href="/manager" className="p-2 border-2 rounded-3xl">
+      <Link
+        href="/manager"
+        className="p-2 border-2 border-neutral-300 rounded-3xl"
+      >
         Manager
       </Link>
     </div>
