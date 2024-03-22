dbf797b1dad580ceaf5fcc3ff31712f1a2168556
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-22 11:31:08 +0700

    e

--- components/Home/Post/AboutEventButton.tsx
+++ components/Home/Post/AboutEventButton.tsx
@@ -9,7 +9,7 @@ function AboutEventButton({ eventPosterURL }: { eventPosterURL: string }) {
       <DialogTrigger className="bg-neutral-800 rounded-xl px-3 py-1 flex items-center justify-center">
         info
       </DialogTrigger>
-      <DialogContent className="max-w-sm aspect-[3/4] p-0 w-full bg-transparent  border-none">
+      <DialogContent className="max-w-sm aspect-square p-0 w-full bg-transparent  border-none">
         <div className="overflow-hidden rounded-2xl bg-neutral-900">
           <Image
             width={500}
