f3f7c1a3e20a40955dfea852f810226da17f1734
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-25 18:30:28 +0700

    e

--- app/(user)/boost/page.tsx
+++ app/(user)/boost/page.tsx
@@ -1,17 +1,18 @@
+import BoostCard from "@/components/Boost/BoostCard";
 import React from "react";
 
 function page() {
   return (
     <div
-      style={{
-        backgroundImage: `url("/bg/boost-bg.jpg")`,
-        backgroundPosition: "left",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-      className="bg min-h-screen"
+      // style={{
+      //   backgroundImage: `url("/bg/boost-bg.jpg")`,
+      //   backgroundPosition: "left",
+      //   backgroundRepeat: "no-repeat",
+      //   backgroundSize: "cover",
+      // }}
+      className="bg-gradient-to-t from-orange-200 to-orange-50 min-h-screen px-4 pt-40"
     >
-      page
+      <BoostCard />
     </div>
   );
 }
