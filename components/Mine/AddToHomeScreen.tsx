1d6b0c522f1ec846abecac497805cf1e4aacc731
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-25 08:37:25 +0700

    e

--- components/Mine/AddToHomeScreen.tsx
+++ components/Mine/AddToHomeScreen.tsx
@@ -3,15 +3,27 @@ import React, { useEffect, useState } from "react";
 
 function AddToHomeScreen() {
   const [isIOS, setIsIOS] = useState(false);
+  const [isPWAInstalled, setIsPWAInstalled] = useState(false); // Thêm state để kiểm tra PWA đã được cài đặt hay chưa
 
   useEffect(() => {
     const userAgent = window.navigator.userAgent.toLowerCase();
     setIsIOS(/iphone|ipad|ipod/.test(userAgent));
+
+    // Kiểm tra nếu đã cài đặt PWA
+    window.addEventListener("beforeinstallprompt", (event) => {
+      event.preventDefault();
+      setIsPWAInstalled(true);
+    });
+
+    // Xoá event listener khi component unmount
+    return () => {
+      window.removeEventListener("beforeinstallprompt", () => {});
+    };
   }, []);
 
   return (
     <>
-      {isIOS && (
+      {isIOS && !isPWAInstalled && (
         <div className="fixed top-28 left-1/2 -translate-x-1/2 w-fit  text-sm bg-white text-neutral-800 px-5 py-2 rounded-3xl flex flex-col items-center justify-center">
           <div className="flex items-center gap-x-[3px] text-nowrap">
             <span>Tap</span>
