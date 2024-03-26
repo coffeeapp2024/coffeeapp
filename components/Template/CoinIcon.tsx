0333d3553d4bd000ab9b9af62bbb1088d6c34a5c
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-27 06:05:52 +0700

    e

--- components/Template/CoinIcon.tsx
+++ components/Template/CoinIcon.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function CoinIcon({ classname }: { classname: string }) {
   return (
     <Image
-      src="/icons/coin.png"
+      src="/icons/coinf.png"
       width={100}
       height={100}
       className={`relative ${classname} mx-[2px] object-contain`}
