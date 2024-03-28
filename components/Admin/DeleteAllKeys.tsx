d83ab1a3fc0537c8e5ba8035fc6da723e3568196
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-29 05:25:05 +0700

    e

--- components/Admin/DeleteAllKeys.tsx
+++ components/Admin/DeleteAllKeys.tsx
@@ -1,25 +1,21 @@
 import React from "react";
 import MainButton from "../MainButton";
-import { deleteAllKeysFromFirestore } from "@/lib/firebaseFunctions";
 import { toast } from "sonner";
 import { useUserDataStore } from "@/store/zustand";
+import { deleteAllDocumentsInCollection } from "@/lib/firebaseUtils";
 
 function DeleteAllKeys() {
   const { role } = useUserDataStore();
 
   const handleDeleteAllKeys = async () => {
     try {
-      await toast.promise(
-        deleteAllKeysFromFirestore(), // Call the deletion function within toast.promise
-        {
-          loading: "Deleting...", // Display loading message
-          success: "All keys deleted successfully", // Display success message
-          error: (error) => error.message, // Display error message
-        }
-      );
+      await toast.promise(deleteAllDocumentsInCollection("keys"), {
+        loading: "Deleting...",
+        success: "All keys deleted successfully",
+        error: (error) => error.message,
+      });
     } catch (error) {
       console.error("Error deleting keys:", error);
-      // Handle error if necessary (e.g., display an error message)
     }
   };
 
