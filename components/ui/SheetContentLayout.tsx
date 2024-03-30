82ce14660984dc9980f5635294093813749a7d94
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-31 02:53:34 +0700

    e

--- components/ui/SheetContentLayout.tsx
+++ components/ui/SheetContentLayout.tsx
@@ -9,9 +9,9 @@ export default function SheetContentLayout({
   className?: string;
 }) {
   return (
-    <SheetContent className={`w-full pt-14 px-4 bg-neutral-50 ${className}`}>
+    <SheetContent className={`w-full pt-14 px-4 bg-background ${className}`}>
       <SheetClose className="fixed top-4 left-4">
-        <button className="bg-white p-3 rounded-xl text-neutral-500">
+        <button className="bg-white bg-opacity-95 shadow-sm p-3 rounded-xl text-neutral-500">
           <ChevronLeftIcon className="w-4 h-4" />
         </button>
       </SheetClose>
