7bf525689af0dc8f4e7a61265463b2ebcd7b70a0
triminhdev <triminhnguyen.dev@gmail.com>
2024-03-31 02:13:41 +0700

    e

--- components/Template/AppCardTemplate.tsx
+++ components/Template/AppCardTemplate.tsx
@@ -20,8 +20,8 @@ function AppCardTemplate({
   const BackgroundIcon = ["bg-violet-400", "bg-yellow-400"];
 
   return (
-    <div className="col-span-2 w-full aspect-square rounded-3xl bg-white border-neutral-200 border-1px shadow-sm overflow-hidden flex flex-col justify-end">
-      <div className="h-full bg-neutral-50">
+    <div className="col-span-2 w-full aspect-square rounded-3xl shadow-sm overflow-hidden flex flex-col justify-end">
+      <div className="h-full bg-orange-50">
         <Image
           src={imageURL}
           width={500}
@@ -30,7 +30,7 @@ function AppCardTemplate({
           className="object-cover w-full h-full "
         />
       </div>
-      <div className="w-full bg-neutral-100 px-3 py-3 flex justify-between items-center">
+      <div className="w-full bg-orange-50 px-3 py-3 flex justify-between items-center">
         <div className="flex gap-x-3">
           <div
             className={`${background} h-12 aspect-square rounded-2xl flex items-center justify-center`}
