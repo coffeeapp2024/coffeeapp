6f5f20cb7425191b025aa9d21f03fa53a7565e52
triminhdev <triminhnguyen.dev@gmail.com>
2024-04-01 15:41:16 +0700

    e

--- components/Boost/Fireplace.tsx
+++ components/Boost/Fireplace.tsx
@@ -12,8 +12,8 @@ const textInner = "Better Fireplace boost mining speech";
 
 const fireplaceIcons = [
   <FireIcon key="1" className={`${iconSize} text-rose-500`} />,
-  <FireIcon key="1" className={`${iconSize} text-pink-500`} />,
   <FireIcon key="1" className={`${iconSize} text-orange-500`} />,
+  <FireIcon key="1" className={`${iconSize} text-cyan-500`} />,
   <FireIcon key="1" className={`${iconSize} text-purple-600`} />,
   <FireIcon key="2" className={`${iconSize} text-red-600`} />,
 ];
