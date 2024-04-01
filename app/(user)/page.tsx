d979f1445986ad630e858d6eab0e24bbae892496
triminhdev <triminhnguyen.dev@gmail.com>
2024-04-02 04:05:07 +0700

    e

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -14,13 +14,13 @@ export default function Home() {
     <main className="relative h-screen max-w-screen-sm z-10">
       <AddToHomeScreen />
       <InstallButton />
-      <Background />
+      {/* <Background /> */}
       <div className="pt-4 pb-20">
         <ProfileCard />
       </div>
       <CurrentCoinInfo />
       <ScanClaimCard />
-      {/* <Tiktok /> */}
+      <Tiktok />
     </main>
   );
 }
