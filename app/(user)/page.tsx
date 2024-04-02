49f26c6a5334f83d7f09335da7455af1b167bdcb
triminhdev <triminhnguyen.dev@gmail.com>
2024-04-02 16:29:06 +0700

    e

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -11,16 +11,22 @@ import Tiktok from "@/components/Mine/Tiktok";
 
 export default function Home() {
   return (
-    <main className="relative h-screen max-w-screen-sm z-10">
+    <main
+      style={{
+        backgroundImage: `url("/bg/mine-bg.jpg")`,
+        backgroundSize: "cover",
+      }}
+      className="relative h-screen max-w-screen-sm z-10"
+    >
       <AddToHomeScreen />
       <InstallButton />
-      <Background />
+      {/* <Background /> */}
       <div className="pt-4 pb-20">
         <ProfileCard />
       </div>
       <CurrentCoinInfo />
       <ScanClaimCard />
-      {/* <Tiktok /> */}
+      <Tiktok />
     </main>
   );
 }
