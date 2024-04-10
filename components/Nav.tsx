e8e737f4a6e1ee6a2d055a1ea95b4ad402c9a7ed
triminhdev <triminhnguyen.dev@gmail.com>
2024-04-10 14:02:53 +0700

    style: move navbar higher

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -28,7 +28,7 @@ function Nav() {
 
   return (
     <div className="fixed z-50 bottom-0 left-0 w-full">
-      <nav className="px-2 pt-2 pb-2 w-full mx-auto flex justify-around items-center bg-background bg-opacity-95 rounded-t-2xl shadow-sm">
+      <nav className="px-2 pt-2 pb-6 w-full mx-auto flex justify-around items-center bg-background bg-opacity-95 shadow-sm">
         {navList.map(([title, url], index) => {
           const isActive = pathname === url;
           return (
