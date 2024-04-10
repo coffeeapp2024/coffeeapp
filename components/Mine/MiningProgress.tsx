b4c3bd219fa697709e5993c6a1d4e98316da9022
triminhdev <triminhnguyen.dev@gmail.com>
2024-04-10 15:25:29 +0700

    e

--- components/Mine/MiningProgress.tsx
+++ components/Mine/MiningProgress.tsx
@@ -17,8 +17,15 @@ function MiningProgress() {
         startTimeMine,
         endTimeMine
       );
-      setMiningProgressPercentage(percentage || 100);
-    }, 1000);
+      setMiningProgressPercentage((percentage || 99) + 1);
+    }, 10000);
+
+    // Set initial percentage immediately after component mounts
+    const initialPercentage = calculateMiningProgressPercentage(
+      startTimeMine,
+      endTimeMine
+    );
+    setMiningProgressPercentage((initialPercentage || 99) + 1);
 
     return () => clearInterval(interval);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -31,7 +38,9 @@ function MiningProgress() {
     >
       <Progress.Indicator
         className="h-full w-full flex-1 bg-gradient-to-r from-yellow-300 to-orange-300 transition-all"
-        style={{ transform: `translateX(-${100 - miningProgressPercentage}%)` }}
+        style={{
+          transform: `translateX(-${100 - miningProgressPercentage}%)`,
+        }}
       />
     </Progress.Root>
   );
